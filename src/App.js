be993169eee59c1e5c8bbf532524df0bf566a17e
Alexandre Jr <alejunior@msn.com>
2022-03-11 18:53:03 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { observer } from 'mobx-react-lite'
 
-function App() {
+const App = observer((props) => {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Contador</h1>
+      <p>Valor: {props.store.count}</p>
+      <button type="button" onClick={() => props.store.increment()}>Incrementar</button>
+      <button type="button" onClick={() => props.store.decrement()}>Decrementar</button>
     </div>
-  );
-}
+  )
+})
 
 export default App;
